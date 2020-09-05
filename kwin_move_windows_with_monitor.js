00e1c9cf0869c239057f7701ecd2025e8f2ff23b
Ian Wrzesinski <n3wance@gmail.com>
2020-09-05 13:43:15 -0400

    reordered functions and removed x variable

--- kwin_move_windows_with_monitor.js
+++ kwin_move_windows_with_monitor.js
@@ -1,28 +1,31 @@
 
-workspace.screenResized.connect(move_if_2160);
-
-function move_if_2160() {
-    var h = workspace.workspaceHeight;
-    print(h);
-    // move down if new height tall
-    if (h == 2160) {
-        move_clients(0, 1080);
-    }
-}
-
-// move_clients(0, 1080);
-function move_clients(x, y) {
+function move_clients(yval) {
     var clients = workspace.clientList();
     for (var i=0; i<clients.length; i++) {
         var client = clients[i];
         if (client.caption != "Plasma") {
             print(client.caption);
             client.geometry = {
-                x: client.x,+ x
-                y: client.y + y,
+                x: client.x,
+                y: client.y + yval,
                 width: client.width,
                 height: client.height
             };
         }
     }
 }
+
+function move_if_2160() {
+    var h = workspace.workspaceHeight;
+    print(h);
+    // move down if new height tall
+    if (h == 2160) {
+        move_clients(1080);
+    }
+}
+
+move_clients(5);
+
+// workspace.screenResized.connect(move_if_2160);
+
+
